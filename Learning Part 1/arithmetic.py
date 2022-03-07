a507ae2c0f910999603d7eeaac01850837886c7b
asikkolorob <asikkolorob@gmail.com>
2022-03-07 11:16:20 +0600

    finaly create project for input()

---
+++ Learning Part 1/arithmetic.py
@@ -0,0 +1,9 @@
+# Project Input()
+
+number1 = input("please type an integer & press enter: ")
+number2 = input("Please type another integer & press enter: ")
+
+number1 = int(number1) # convert String to Integer
+number2 = int(number2) # convert String to Integer
+
+print("number1 + number2 = ", number1 + number2)
