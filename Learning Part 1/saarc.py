ef3c3bfe595d20ed37468552af0160f3d7bb3994
asikkolorob <asikkolorob@gmail.com>
2022-03-09 12:12:53 +0600

    code Sumthing Changed

--- Learning Part 1/saarc.py
+++ Learning Part 1/saarc.py
@@ -1,9 +1,12 @@
-saarc = ["Bangladesh", "Afghanistan", "Bhutam", "Nepal", "India", "Pakistan", "SriLanka"]
+saarc = ["Bangladesh", "Afghanistan", "Bhutam", "Nepal", "India", 
+"Pakistan", "SriLanka"] # List of veriable
 
-country = input("Enter Name of the country: ")
+country = input("Enter Name of the country: ") #buit in Function input()
 
-if country in saarc:
-    print(country, "is a member of SAARC")
+if country in saarc:  # if statement
+    print(country , "is a member of SAARC")
+else: # else Statement
+    print(country, "is Not a member of SAARC")
 
 print("Program termened!")
 
