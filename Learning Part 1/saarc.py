4cfd03cf34e74105e0d040f85dbfcab300bfa287
asikkolorob <asikkolorob@gmail.com>
2022-03-09 12:00:14 +0600

    SAARC Program complated

---
+++ Learning Part 1/saarc.py
@@ -0,0 +1,9 @@
+saarc = ["Bangladesh", "Afghanistan", "Bhutam", "Nepal", "India", "Pakistan", "SriLanka"]
+
+country = input("Enter Name of the country: ")
+
+if country in saarc:
+    print(country, "is a member of SAARC")
+
+print("Program termened!")
+
